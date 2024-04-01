d8e5773b28e2a7cdc8a7b47bf216773a6fe0e6db
Govinda010 <gsth010@gmail.com>
2024-04-01 21:57:27 +0200

    started the section 5, Html is completed

---
+++ 01-Fundamentals/script.js
@@ -0,0 +1,14 @@
+const firstName = 'Govinda';
+const job = 'Programmer';
+const birthYear = '1992';
+const year = 2024
+
+const govinda = "I'm " + firstName + ',a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(govinda)
+
+const govindaNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(govindaNew);
+
+console.log(`String
+multiple
+lines`);
